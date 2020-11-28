a4b696ae5ff56b6b701a70d2e3bfefd7df63b056
Jaqueline <jaquelinesv90@gmail.com>
2020-11-28 01:00:48 -0300

    added comments in Map class

--- src/com/collections/map/Main.java
+++ src/com/collections/map/Main.java
@@ -1,9 +1,25 @@
 package com.collections.map;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /* 
+ * Map<K,V>
+ * 
+ * K - the type of keys maintened by this map.
+ * V - the type of mapped values. 
+ * 
+ * - A map cannot contain duplicate keys;
+ * - is implemented by different Java classes, such as HashMap,
+ * HashTable, TreeMap.
+ * 
+ * famous methods: KeySet: Returns a Set that contains all keys of the map.
  * 
  * 
+ * example to use: phone book
  */
 public class Main {
+	
+	Map<String,Integer> map = new HashMap<>();
 
 }
